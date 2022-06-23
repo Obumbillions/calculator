d6e42b7d46abcc78b4e00b483e33507f5d8916a8
Obumbillions <obumnemeokoy914@gmail.com>
2022-06-23 14:43:03 +0100

    Ch: Added the codes and structures

---
+++ script.js
@@ -0,0 +1,19 @@
+function addition(a, b){
+    return a + b;
+}
+console.log(addition(1, 2));
+
+function subtract(a, b){
+    return a - b;
+}
+console.log(subtract(1, 2));
+
+function multiply(a, b){
+    return a * b;
+}
+console.log(multiply(1, 2));
+
+function division(a, b){
+    return a / b;
+}
+console.log(division(1, 2));
